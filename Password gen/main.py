cfea2b52e3b128fff03d0c010dea8c3a9ae0dc43
Bourdon94m <105060610+Bourdon94m@users.noreply.github.com>
2022-12-08 21:28:20 +0100

    Add files via upload

---
+++ Password gen/main.py
@@ -0,0 +1,30 @@
+import random
+
+
+
+def welcome_message():
+    print("______                     _                 _____           _       _   ")
+    print("| ___ \                   | |               /  ___|         (_)     | |  ")
+    print("| |_/ / ___  _   _ _ __ __| | ___  _ __     \ `--.  ___ _ __ _ _ __ | |_ ")
+    print("| ___ \/ _ \| | | | '__/ _` |/ _ \| '_ \     `--. \/ __| '__| | '_ \| __|")
+    print("| |_/ / (_) | |_| | | | (_| | (_) | | | |   /\__/ / (__| |  | | |_) | |_ ")
+    print("\____/ \___/ \__,_|_|  \__,_|\___/|_| |_|   \____/ \___|_|  |_| .__/ \__|")
+    print()
+    print()
+    print()
+    print()
+welcome_message()
+
+
+def entry_message():
+    # VARIABLES 
+    choice_str = input("1 - 2 - 3 - 4")
+    choice_int = int(choice_str)
+
+    
+   
+        
+
+        
+        
+entry_message()
